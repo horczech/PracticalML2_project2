1291003b2537331083688c393bb05755290511fa
Martin Horak <horczech@gmail.com>
2019-01-24 10:04:36 +0100

    remove resizing and image normalization

--- data_generator.py
+++ data_generator.py
@@ -58,8 +58,8 @@ class DataGenerator(keras.utils.Sequence):
             img_path = str(data_info['image_path'])
             img = cv2.imread(img_path)
             # ToDo: This is not probably a best place to resize the image and image
-            img = cv2.resize(img, (self.X_shape[0], self.X_shape[1]))
-            img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+            # img = cv2.resize(img, (self.X_shape[0], self.X_shape[1]))
+            # img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
             # load labels (result)
             target = self.parse_object_data(data_info['objects'])
