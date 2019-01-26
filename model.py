0710f34299099f3b33ba09b023caa6dcde595722
Martin Horak <horczech@gmail.com>
2019-01-26 01:02:39 +0100

    >>

--- model.py
+++ model.py
@@ -53,7 +53,7 @@ class YOLO:
             # optimizer = optimizers.SGD(lr=1e-16, decay=1e-6, nesterov=True)
 
             optimizer = optimizers.Adam(lr=learning_rate)
-            self.model.compile(loss=self.custom_loss, optimizer=optimizer, metrics=['accuracy'])
+            self.model.compile(loss=self.custom_loss, optimizer=optimizer)
 
         ################################
         # Create data generators
